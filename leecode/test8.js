3f98c841519a67bdd4bb0abf8923b54cba4805b0
sweetamily <972041628@qq.com>
2019-02-24 22:34:03 +0800

    update

--- leecode/test8.js
+++ leecode/test8.js
@@ -12,9 +12,15 @@
 var sumNumbers = function(root) {
     if(root == null)
         return 0;
-    sumNodes(root,0)
+    return sumNodes(root,0)
 };
 
 var sumNodes = (root, sum)=>{
-    
+    sum = sum * 10 + root.val;
+    if(root.left == null && root.right == null){
+        return sum;
+    }
+    var left = root.left == null ? 0 : sumNodes(root.left, sum);
+    var right = root.right == null ? 0 : sumNodes(root.right, sum);
+    return left + right;
 }
