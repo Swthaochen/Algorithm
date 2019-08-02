386e4257c9caeef67d9858d9c4f837574080725d
sweetamily <972041628@qq.com>
2019-08-02 12:51:01 +0800

    suffix_string

---
+++ practice_for_summer2019/suffix_string/suffix_string1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+string s;
+vector<string> sv;  // 存放后缀数组的数组
+vector<int> link;   // 存放排名的数组
+int l;
+
+void sortString(){
+    int i = 2;
+    while(i/2 < l){
+        vector< pair<pair<int,int>,int> > a;  //定义存放名次对的二元组
+        int j;
+        for(j = 0;j + i/2 < l;j++) {
+            pair<int, int> p(link[j], link[j + i / 2]);
+            pair<pair<int, int>, int> q(p, j);
+            a.push_back(q);
+        }
+        for(;j < l;j++){
+            pair<int, int> p(link[j], 0);
+            pair<pair<int, int>, int> q(p, j);
+            a.push_back(q);
+        }
+        sort(a.begin(),a.end());
+        int local = 0;
+        for(j = 0;j < l;j++){
+            if(j != 0 && a[j].first == a[j-1].first){
+                link[a[j].second] = local;
+            }else{
+                link[a[j].second] = ++local;
+            }
+        }
+        i = i * 2;
+    }
+}
+
+int main() {
+    cin >> s;
+    l = s.length();
+    for (int i = 0; i < l; ++i) {
+        sv.push_back(s.substr(i,l-i));
+        link.push_back(s[i]-'a'+1);
+    }
+    sortString();
+    int _link[l];
+    for(int i = 0;i < l;i++){
+        _link[link[i]-1] = i;
+    }
+    for(int i = 0;i < l;i++){
+        cout << sv[_link[i]] << endl;
+    }
+    return 0;
+}
